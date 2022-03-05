e277dd7069c264e177a6e93adf8b3f2f98a55fbe
Arthur Béquié <arthurbequie@gmail.com>
2022-03-05 01:58:43 +0100

    OP11-12: Header component done with success

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HousingProvider } from './utils/context';
+import Header from './layout/Header';
+import './style/main.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<h1>Home</h1>}></Route>
-      <Route path='/housing/:id' element={<h1>Housing</h1>}></Route>
-      <Route path='/about' element={<h1>About</h1>}></Route>
-      <Route path='*' element={<h1>404 not found</h1>}></Route>
-    </Routes>
+    <HousingProvider>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>}></Route>
+        <Route path='/housing/:id' element={<h1>Housing</h1>}></Route>
+        <Route path='/about' element={<h1>About</h1>}></Route>
+        <Route path='*' element={<h1>404 not found</h1>}></Route>
+      </Routes>
+    </HousingProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
