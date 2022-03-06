88fd81d49301599264b807be96955eba0064be43
Arthur Béquié <arthurbequie@gmail.com>
2022-03-06 03:16:18 +0100

    OP11-19/OP11-15: added link to logding sheet on the click

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
+
 interface Props {
+    id: string,
     title: string,
     image: string,
 }
 
 const Card = (props: Props): JSX.Element => {
     return(
-        <article className="card">
-            <h3 className="card-title">{props.title}</h3>
-            <div className="card-layer"/>
-            <img className="card-image" src={props.image} alt={props.title}/>
-        </article>
+        <Link to={`/housing/${props.id}`}>
+            <article className="card">
+                <h3 className="card-title">{props.title}</h3>
+                <div className="card-layer"/>
+                <img className="card-image" src={props.image} alt={props.title}/>
+            </article>
+        </Link>
     );
 };
 
